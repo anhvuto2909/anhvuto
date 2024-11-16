0727034a30ed3429cfdae44ca6b9cd72f52f3d78
To Anh Vu <toanhvu8572@gmail.com>
2024-11-16 21:00:37 +0700

    update3

--- ecom/settings.py
+++ ecom/settings.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load enviroment variables
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -67,11 +71,19 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+        ) }
+{
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432',
     }
-}
 
 
 # Password validation
